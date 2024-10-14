c660abc03adf315fa2fd5da9d91592310d5a3116
crsPerzGonzal <Cperezg22@ul.edu.co>
2024-10-13 19:44:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, HTTPException
 import mysql.connector
-from core.confi import connetion
-from models.user import User
+from backend.core.confi import get_connetion
+from backend.models.user import User
+
+
 app = FastAPI()
 
 
@@ -9,18 +11,22 @@ app = FastAPI()
 def read_root():
     return {"hola" : "mundo"}
 
-@app.get("/user")
-async def login(dato: User):
-    cursor = connetion.cursor(dictionary=True)
-    query = "select * from user where Username = %s and password = %s"
-
+@app.post("/users")
+async def get_user_info(user: User):
+    connection = get_connetion()
+    cursor = connection.cursor(dictionary=True)
+    query = "SELECT * FROM users WHERE username = %s AND password_hash = %s"
+    
     try:
-        cursor.execute(query)
-        users = cursor.fetchall()
-        return users
-    except mysql.connector.Error as err: 
-        raise HTTPException(status_code=500, detail=f'error al conectar con mysql : {err}')
+        cursor.execute(query, (user.username, user.password_hash))
+        user_result = cursor.fetchone()
+        
+        if user_result:
+            return user_result  # Devuelve la información del usuario
+        else:
+            raise HTTPException(status_code=404, detail="Usuario no encontrado o contraseña incorrecta.")
+    except mysql.connector.Error as err:
+        raise HTTPException(status_code=500, detail=f'Error al conectar con MySQL: {err}')
     finally:
         cursor.close()
-
-
+        connection.close()
