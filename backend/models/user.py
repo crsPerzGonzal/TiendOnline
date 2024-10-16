06bb1a8473958397177625cf772b7edb79c54362
crsPerzGonzal <Cperezg22@ul.edu.co>
2024-10-16 15:49:25 -0500

    Update user.py

--- backend/models/user.py
+++ backend/models/user.py
@@ -3,4 +3,15 @@ from pydantic import BaseModel
 class User(BaseModel):
      username: str
      password_hash: str
+
+class regiUser(BaseModel):
+     username: str
+     email:str
+     password_hash: str
      
+class product(BaseModel):
+      id: str
+      name: str
+      price: float
+      image_url: str
+      description: str
