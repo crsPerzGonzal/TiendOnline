9ada5ef0d7ce78607ff2d20cf908d335a1c246b8
crsPerzGonzal <Cperezg22@ul.edu.co>
2024-10-17 21:00:29 -0500

    Update ProductItem.js

--- frontend/src/componentes/ProductItem.js
+++ frontend/src/componentes/ProductItem.js
@@ -6,7 +6,10 @@ import "./ProductItem.css"; // Crea un archivo CSS para estilos específicos
 
 const ProductItem = ({ product }) => {
   const { addToCart } = useContext(CartContext);
-
+  const handleAddToCart = (product) => {
+    console.log("Producto añadido al carrito:", product);
+    addToCart(product);
+  };
   return (
     <div className="product-item">
       <img src={product.image_url} alt={product.name} />
@@ -14,8 +17,13 @@ const ProductItem = ({ product }) => {
       <p>${product.price.toFixed(2)}</p>
       <p>{product.description}</p>
       <div className="buttons">
-        <button onClick={() => addToCart(product)}>Añadir al Carrito</button>
-        <Link to={`/products/${product.id}`}>
+      <button onClick={() => {
+        console.log("Producto añadido:", product); // Aquí se registra el producto en la consola
+        handleAddToCart(product); // Llama a la función para añadir al carrito
+      }}>
+        Añadir al Carrito
+        </button>
+        <Link to={`/product/${product.product_id}`}>
           <button>Ver Producto</button>
         </Link>
       </div>
